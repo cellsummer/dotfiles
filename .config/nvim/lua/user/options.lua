813e5c9dafdb63baeb224c263c26e9549594abdc
Wenjing Fang <fang.wenjing@yahoo.com>
2022-03-25 21:58:55 -0400

    init commit

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,36 @@
+local opts = {
+	backup = false,
+	writebackup = false,
+	swapfile = false,
+	compatible = false,
+	wildmenu = true,
+	ignorecase = true,
+	smartcase = true,
+--	shellslash = true,
+	encoding = "utf-8",
+	relativenumber = true,
+	ts = 4,
+	sw = 4,
+	expandtab = true,
+	hidden = true,
+	hlsearch = true,
+	mouse = "a",
+	nu = true,
+	foldmethod = "indent",
+	foldlevel = 4,
+	inccommand = "split",
+	scrolloff = 10,
+    cursorline = true,
+}
+
+vim.opt.shortmess:append("c")
+
+for k, v in pairs(opts) do
+	vim.opt[k] = v
+end
+
+vim.cmd([[set iskeyword+=-]])
+vim.cmd([[set clipboard+=unnamedplus]])
+--set noeb vb t_vb=
+--set backspace=indent,eol,start
+--set spellsuggest=best,5
---
+++ Readme.md
@@ -0,0 +1,11 @@
+## My Linux dot files
+
+### Neovim
+
+[How I set up my (neo)vim]( https://cellsummer.github.io/views/2022-02-05-My-NeoVim-Config.html )
+
+
+
+### NeoMutt
+
+### Ranger
