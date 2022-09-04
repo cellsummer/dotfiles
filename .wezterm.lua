9f34f869a879c36c895b0bdd965c01d47de2a840
Wenjing Fang <fang.wenjing@yahoo.com>
2022-09-04 14:15:32 -0400

    wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,51 +6,60 @@ return {
         "c:/Program Files/WindowsApps/Microsoft.PowerShell_7.1.3.0_x64__8wekyb3d8bbwe/pwsh.exe",
         "-noLogo"
     },
- -- Appearance
+    -- Appearance
     default_cursor_style = "BlinkingBar",
     font = wezterm.font("Hack"),
-    color_scheme = "tokyonight",
+    -- color_scheme = "tokyonight",
+    color_scheme = "Catppuccin Macchiato",
+    -- color_scheme = "Catppuccin Latte",
     inactive_pane_hsb = {
         saturation = 0.8,
         brightness = 0.4,
-  },
+    },
+    -- quick mode
 
--- Keybindings
-keys = {
-    -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
-    {key="8", mods="CTRL", action=act.PaneSelect},
-    -- activate pane selection mode with numeric labels
-    {key="9", mods="CTRL", action=act.PaneSelect{alphabet="1234567890"}},
-    -- show the pane selection mode, but have it swap the active and selected panes
-    {key="0", mods="CTRL", action=act.PaneSelect{mode="SwapWithActive"}},
+    --quick_select_patterns = {'[a-zA-Z:\\]*', },
 
-    -- window navigation
-    { key = "h", mods="ALT",
-      action=act.ActivatePaneDirection("Left")},
-    { key = "l", mods="ALT",
-      action=act.ActivatePaneDirection("Right")},
-    { key = "k", mods="ALT",
-      action=act.ActivatePaneDirection("Up")},
-    { key = "j", mods="ALT",
-      action=act.ActivatePaneDirection("Down")},
-    { key = "H", mods = "ALT", action=act.AdjustPaneSize{"Left", 5} },
-    { key = "J", mods = "ALT", action=act.AdjustPaneSize{"Down", 5} },
-    { key = "K", mods = "ALT", action=act.AdjustPaneSize{"Up", 5} },
-    { key = "L", mods = "ALT", action=act.AdjustPaneSize{"Right", 5} },
+    -- Keybindings
+    keys = {
+        -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
+        -- {key=";", mods="ALT", action=act.PaneSelect},
+        -- activate pane selection mode with numeric labels
+        { key = ";", mods = "ALT", action = act.PaneSelect },
+        -- show the pane selection mode, but have it swap the active and selected panes
+        -- {key="Space", mods="ALT", action=act.PaneSelect{mode="SwapWithActive"}},
+        -- Toggle max pane
+        { key = 'Space', mods = 'ALT', action = wezterm.action.TogglePaneZoomState },
 
-    -- close tab
-    { key = "q", mods="ALT", action=wezterm.action.CloseCurrentTab{confirm=true}},
-    -- toggle full screen
-    { key = "F11", action=wezterm.action.ToggleFullScreen},
+        -- window navigation
+        { key = "h", mods = "ALT",
+            action = act.ActivatePaneDirection("Left") },
+        { key = "l", mods = "ALT",
+            action = act.ActivatePaneDirection("Right") },
+        { key = "k", mods = "ALT",
+            action = act.ActivatePaneDirection("Up") },
+        { key = "j", mods = "ALT",
+            action = act.ActivatePaneDirection("Down") },
+        { key = "H", mods = "ALT", action = act.AdjustPaneSize { "Left", 5 } },
+        { key = "J", mods = "ALT", action = act.AdjustPaneSize { "Down", 5 } },
+        { key = "K", mods = "ALT", action = act.AdjustPaneSize { "Up", 5 } },
+        { key = "L", mods = "ALT", action = act.AdjustPaneSize { "Right", 5 } },
 
-    -- This will create a new split and run your default program inside it
-    {key="+", mods="ALT|SHIFT",
-      action=wezterm.action.SplitHorizontal{domain="CurrentPaneDomain"}},
+        -- close tab
+        { key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane { confirm = false } },
+        -- toggle full screen
+        { key = "F11", action = wezterm.action.ToggleFullScreen },
 
-    {key="d", mods="ALT|SHIFT",
-      action=wezterm.action.SplitVertical{domain="CurrentPaneDomain"}},
+        -- This will create a new split and run your default program inside it
+        { key = "Enter", mods = "ALT",
+            action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
 
-    -- Create a new tab in the same domain as the current pane.
-    {key="Enter", mods="ALT", action=act.SpawnTab("CurrentPaneDomain")},
-  }
+        { key = "Enter", mods = "ALT|SHIFT",
+            action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+
+        -- Active Copy Mode
+        { key = "v", mods = "ALT", action = act.ActivateCopyMode },
+        -- Create a new tab in the same domain as the current pane.
+        { key = "+", mods = "ALT|SHIFT", action = act.SpawnTab("CurrentPaneDomain") },
+    }
 }
