d32ba3c41f10060db8893f363e7def18ed641789
cellsummer <fang.wenjing@yahoo.com>
2023-04-28 07:56:34 -0400

    AstroVim & zsh

---
+++ zsh/.config/zsh/exportrc.zsh
@@ -0,0 +1,7 @@
+# Environment variables
+eval "$(/opt/homebrew/bin/brew shellenv)"
+export PATH
+export EDITOR=nvim
+export BROWSER=open
+export FZF_DEFAULT_OPTS='--border=rounded --margin=5% --color=dark --height 100% --reverse  --info=hidden --header-first'
+ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
